7c41558d2171127f84ee46b5da1fccf168a3cac3
esaxena <esaxena@expediagroup.com>
2024-03-08 00:11:54 +0530

    Initial commit

---
+++ src/main/kotlin/com/example/feedbackbackend/FeedbackBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.feedbackbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FeedbackBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<FeedbackBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/feedbackbackend/FeedbackBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.feedbackbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FeedbackBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
