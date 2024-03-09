7eb6b89d66f8d3d2c172bf334574c87f8225a420
esaxena <esaxena@expediagroup.com>
2024-03-09 22:39:52 +0530

    Whitelist Cross origin requests

--- src/main/kotlin/com/example/feedbackbackend/controller/FeedbackController.kt
+++ src/main/kotlin/com/example/feedbackbackend/controller/FeedbackController.kt
@@ -4,6 +4,7 @@ import com.example.feedbackbackend.model.Feedback
 import com.example.feedbackbackend.service.FeedbackService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin
 @RestController
 class FeedbackController(
     val feedbackService: FeedbackService
